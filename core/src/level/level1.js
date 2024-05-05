927f42a11c0be8ed187c1a7af8e6deddf7027d25
t0blas <spx0420@protonmail.com>
2024-05-05 21:55:48 +0200

    collision debug

--- core/src/level/level1.js
+++ core/src/level/level1.js
@@ -54,13 +54,142 @@ level1.elementList.add(new SolidBlock(0.4, 388 - 32 * 6, 1, 2))
 level1.elementList.add(new SolidBlock(0, 388 - 32 * 3, 2, 1))
 level1.elementList.add(new SolidBlock(32 * 5, 388 - 32 * 6, 2, 2))
 
-// level1.elementList.add(new SolidBlock(32 * 8, 388 - 32 * 0, 1, 1))
-// level1.elementList.add(new SolidBlock(32 * 8, 388 - 32 * 1, 1, 1))
-// level1.elementList.add(new SolidBlock(32 * 8, 388 - 32 * 2, 1, 1))
-// level1.elementList.add(new SolidBlock(32 * 8, 388 - 32 * 3, 1, 1))
-// level1.elementList.add(new SolidBlock(32 * 8, 388 - 32 * 4, 1, 1))
-// level1.elementList.add(new SolidBlock(32 * 8, 388 - 32 * 5, 1, 1))
+level1.elementList.add(new SolidBlock(32 * 10, 388 - 32 * 0, 1, 1))
+level1.elementList.add(new SolidBlock(32 * 10, 388 - 32 * 1, 1, 1))
+level1.elementList.add(new SolidBlock(32 * 10, 388 - 32 * 2, 1, 1))
+level1.elementList.add(new SolidBlock(32 * 10, 388 - 32 * 3, 1, 1))
+level1.elementList.add(new SolidBlock(32 * 10, 388 - 32 * 4, 1, 1))
+level1.elementList.add(new SolidBlock(32 * 10, 388 - 32 * 5, 1, 1))
 
 level1.elementList.add(new JumpPad(256, 384, 1, 1))
 
 level1.elementList.add(new Player(32, 64, level1))
+
+level1.elementList.add(
+    new SolidBlock(
+        getRandomInt(0, 1500),
+        getRandomInt(0, 384),
+        getRandomInt(1, 5),
+        getRandomInt(1, 5)
+    )
+)
+level1.elementList.add(
+    new SolidBlock(
+        getRandomInt(0, 700),
+        getRandomInt(0, 384),
+        getRandomInt(1, 5),
+        getRandomInt(1, 5)
+    )
+)
+level1.elementList.add(
+    new SolidBlock(
+        getRandomInt(0, 700),
+        getRandomInt(0, 384),
+        getRandomInt(1, 5),
+        getRandomInt(1, 5)
+    )
+)
+level1.elementList.add(
+    new SolidBlock(
+        getRandomInt(0, 700),
+        getRandomInt(0, 384),
+        getRandomInt(1, 5),
+        getRandomInt(1, 5)
+    )
+)
+level1.elementList.add(
+    new SolidBlock(
+        getRandomInt(0, 1500),
+        getRandomInt(0, 384),
+        getRandomInt(1, 5),
+        getRandomInt(1, 5)
+    )
+)
+level1.elementList.add(
+    new SolidBlock(
+        getRandomInt(0, 700),
+        getRandomInt(0, 384),
+        getRandomInt(1, 5),
+        getRandomInt(1, 5)
+    )
+)
+level1.elementList.add(
+    new SolidBlock(
+        getRandomInt(0, 700),
+        getRandomInt(0, 384),
+        getRandomInt(1, 5),
+        getRandomInt(1, 5)
+    )
+)
+level1.elementList.add(
+    new SolidBlock(
+        getRandomInt(0, 700),
+        getRandomInt(0, 384),
+        getRandomInt(1, 5),
+        getRandomInt(1, 5)
+    )
+)
+level1.elementList.add(
+    new SolidBlock(
+        getRandomInt(0, 1500),
+        getRandomInt(0, 384),
+        getRandomInt(1, 5),
+        getRandomInt(1, 5)
+    )
+)
+level1.elementList.add(
+    new SolidBlock(
+        getRandomInt(0, 700),
+        getRandomInt(0, 384),
+        getRandomInt(1, 5),
+        getRandomInt(1, 5)
+    )
+)
+level1.elementList.add(
+    new SolidBlock(
+        getRandomInt(0, 700),
+        getRandomInt(0, 384),
+        getRandomInt(1, 5),
+        getRandomInt(1, 5)
+    )
+)
+level1.elementList.add(
+    new SolidBlock(
+        getRandomInt(0, 700),
+        getRandomInt(0, 384),
+        getRandomInt(1, 5),
+        getRandomInt(1, 5)
+    )
+)
+level1.elementList.add(
+    new SolidBlock(
+        getRandomInt(0, 1500),
+        getRandomInt(0, 384),
+        getRandomInt(1, 5),
+        getRandomInt(1, 5)
+    )
+)
+level1.elementList.add(
+    new SolidBlock(
+        getRandomInt(0, 700),
+        getRandomInt(0, 384),
+        getRandomInt(1, 5),
+        getRandomInt(1, 5)
+    )
+)
+level1.elementList.add(
+    new SolidBlock(
+        getRandomInt(0, 700),
+        getRandomInt(0, 384),
+        getRandomInt(1, 5),
+        getRandomInt(1, 5)
+    )
+)
+level1.elementList.add(
+    new SolidBlock(
+        getRandomInt(0, 700),
+        getRandomInt(0, 384),
+        getRandomInt(1, 5),
+        getRandomInt(1, 5)
+    )
+)
