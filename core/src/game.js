b649e1de92194bd6efd246b5aa59126170c1b2fa
t0blas <spx0420@protonmail.com>
2024-06-01 16:22:46 +0200

    changed level initiliasation to enable giving game as a parameter to the elements

--- core/src/game.js
+++ core/src/game.js
@@ -1,5 +1,4 @@
-import LevelList from "./level/levelList"
-import { level1 } from "./level/level1"
+import Level from "./level/level"
 import Camera from "./camera/camera"
 import UILayer from "./ui/uiLayer"
 import { Screen, currentScreen } from "./listener/store"
@@ -14,7 +13,8 @@ export default class Game {
 
         this.raf = null
 
-        this.levelList = new LevelList()
+        // first argument is name, second is gravity constant
+        this.level = new Level("level1", 0.8, this)
 
         this.menu = new Menu(this.canvas, this.ctx, this)
 
@@ -25,7 +25,7 @@ export default class Game {
         this.then = this.startTime
         this.raf = window.requestAnimationFrame(this.draw.bind(this))
 
-        this.level = level1
+        this.level.initLevel("level1")
 
         this.player = this.level.getPlayer()
 
--- core/src/level/level1.js
+++ core/src/level/level1.js
@@ -11,23 +11,3 @@ import Level from "./level"
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * max) + min
 }
-
-// first argument is name, second is gravity constant
-export const level1 = new Level("Level 1", 0.8)
-
-level1.elementList.add(new Player(0, 0, level1))
-level1.elementList.add(new SolidBlock(0, 256, 100, 1))
-
-level1.elementList.add(new JumpPad(200, 224, 1, 1))
-level1.elementList.add(new TemporaryBlock(232, 224, 1, 1))
-level1.elementList.add(new Checkpoint(264, 224, 1, 1))
-
-level1.elementList.add(new SolidBlock(364, 128, 10, 1))
-level1.elementList.add(new SolidBlock(32, 96, 3, 1))
-
-level1.elementList.add(new SolidBlock(250, 50, 3, 1))
-
-level1.elementList.add(new SolidBlock(600, 32, 10, 1))
-level1.elementList.add(new SolidBlock(600, 0, 10, 1))
-
-level1.elementList.add(new MovingPlatform(0, 140, 3, 1, 5, 0))
