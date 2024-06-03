b95ee28426df6fb8c86088d692df7684d3ca9a5f
micdonas <dawoodmike2@gmail.com>
2024-06-03 17:28:26 +0200

    Spike animation

--- core/src/element/spike.js
+++ core/src/element/spike.js
@@ -1,8 +1,14 @@
 import Element from "./element"
+import Sprite from "./sprite"
 
 export default class Spike extends Element {
     constructor(x, y, game, relativeWidth = 1, relativeHeight = 1) {
         super(x, y + 7, relativeWidth, relativeHeight)
+
+        this.spikes = new Sprite("spikes/spikes2.png", this.width, this.height, 70, 70)
+        this.currentSprite = this.spikes
+        this.currentFrame = 0
+        this.elapsedFrames = 0
     }
 
     handleCollisionX(player) {
@@ -18,10 +24,20 @@ export default class Spike extends Element {
     }
 
     draw(ctx) {
+        this.updateFrames()
+        this.currentSprite.draw(ctx, this.currentFrame, 0, this.position)
         ctx.beginPath()
-        ctx.rect(this.position.x, this.position.y, this.width, 25)
+        // ctx.rect(this.position.x, this.position.y, this.width, 25)
         ctx.fillStyle = "white"
         ctx.fill()
         ctx.closePath()
+    }
+
+    updateFrames() {
+        this.elapsedFrames++
+        if (this.elapsedFrames % 11 === 0) {
+            if (this.currentFrame < 7 - 1) this.currentFrame++
+            else this.currentFrame = 0
+        }
     }
 }
