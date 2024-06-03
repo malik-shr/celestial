2fe973d699891157b7dca1116492c7f1f0f9cca9
micdonas <dawoodmike2@gmail.com>
2024-06-03 22:50:45 +0200

    Spikes und jumpPad animation fixes

--- core/src/element/spike.js
+++ core/src/element/spike.js
@@ -9,28 +9,32 @@ export default class Spike extends Element {
         this.currentSprite = this.spikes
         this.currentFrame = 0
         this.elapsedFrames = 0
+        this.activeFrames = 0
     }
 
     handleCollisionX(player) {
         if (!player.isDead) {
-            player.die()
+            if (this.activeFrames === 4) {
+                this.activeFrames = 0
+                player.die()
+            }
         }
     }
 
     handleCollisionY(player) {
         if (!player.isDead) {
-            player.die()
+            ++this.activeFrames
+
+            if (this.activeFrames === 3) {
+                this.activeFrames = 0
+                player.die()
+            }
         }
     }
 
     draw(ctx) {
         this.updateFrames()
         this.currentSprite.draw(ctx, this.currentFrame, 0, this.position)
-        ctx.beginPath()
-        // ctx.rect(this.position.x, this.position.y, this.width, 25)
-        ctx.fillStyle = "white"
-        ctx.fill()
-        ctx.closePath()
     }
 
     updateFrames() {
