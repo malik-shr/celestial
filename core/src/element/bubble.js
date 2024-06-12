6fcbfa9bedd9c0415ce363d44151929592238de6
micdonas <dawoodmike2@gmail.com>
2024-06-12 19:36:28 +0200

    Coin sprite

--- core/src/element/bubble.js
+++ core/src/element/bubble.js
@@ -8,7 +8,7 @@ export default class Bubble extends Element {
         this.collided = false
         this.cooldown = 0
 
-        this.sprite = new Sprite("temporary_block.png", 32, 32, 32, 32)
+        this.sprite = new Sprite("bubble.png", 32, 32, 70, 70)
     }
 
     action() {
@@ -49,16 +49,18 @@ export default class Bubble extends Element {
     draw(ctx) {
         if (this.cooldown > 20) return
 
-        ctx.beginPath()
-        ctx.arc(
-            this.position.x + this.width / 2,
-            this.position.y + this.height / 2,
-            this.width / 2,
-            0,
-            Math.PI * 2
-        )
-        ctx.fillStyle = "rgba(255,255,255,0.2)"
-        ctx.fill()
-        ctx.closePath()
+        this.sprite.draw(ctx, 0, 0, this.position)
+        // ctx.beginPath()
+        // ctx.arc(
+        //     this.position.x + this.width / 2,
+        //     this.position.y + this.height / 2,
+        //     this.width / 2,
+        //     0,
+        //     Math.PI * 2
+        // )
+
+        // ctx.fillStyle = "rgba(255,255,255,0.2)"
+        // ctx.fill()
+        // ctx.closePath()
     }
 }
