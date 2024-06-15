cd08b21df2add69073233fc94970efa8308a5e7a
micdonas <dawoodmike2@gmail.com>
2024-06-15 18:03:31 +0200

    flags for checkpoints and endgoal

--- core/src/element/checkpoint.js
+++ core/src/element/checkpoint.js
@@ -1,11 +1,14 @@
 import Element from "./element"
-
+import Sprite from "./sprite"
 export default class Checkpoint extends Element {
     constructor(x, y, game) {
         super(x, y)
 
         this.game = game
         this.isActive = false
+        this.checkPointRed = new Sprite("checkPoint.png", this.width, this.height, 70, 70)
+        this.checkPointGreen = new Sprite("checkPointGreen.png", this.width, this.height, 70, 70)
+        this.currentSprite = this.checkPointRed
     }
 
     handleCollisionX(player) {
@@ -33,10 +36,16 @@ export default class Checkpoint extends Element {
     }
 
     draw(ctx) {
-        ctx.beginPath()
-        ctx.rect(this.position.x, this.position.y, this.width, this.height)
-        ctx.fillStyle = "yellow"
-        ctx.fill()
-        ctx.closePath()
+        if (this.isActive) {
+            this.currentSprite = this.checkPointGreen
+        } else this.currentSprite = this.checkPointRed
+
+        this.currentSprite.draw(ctx, this.currentFrame, 0, this.position)
+
+        // ctx.beginPath()
+        // ctx.rect(this.position.x, this.position.y, this.width, this.height)
+        // ctx.fillStyle = "yellow"
+        // ctx.fill()
+        // ctx.closePath()
     }
 }
