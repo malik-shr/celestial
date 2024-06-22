a018ef38e393c0ec49fac5370951e88d4a34563c
t0blas <spx0420@protonmail.com>
2024-06-22 11:32:02 +0200

    added dance button ("d") and goal dance at the end

--- core/src/element/goal.js
+++ core/src/element/goal.js
@@ -7,26 +7,43 @@ export default class Goal extends Element {
 
         this.game = game
         this.isActive = false
+        this.counter = 0
 
         this.sprite = new Sprite("rocket.png", 32, 96, 32, 96)
     }
 
+    action() {
+        if (this.isActive) {
+            this.counter += 1
+        }
+
+        if (this.isActive) {
+            if (Math.floor(this.counter / 25) % 2 === 0) {
+                this.game.player.currentSprite = this.game.player.sprites.jump.right
+            } else {
+                this.game.player.currentSprite = this.game.player.sprites.jump.left
+            }
+        }
+
+        if (this.counter >= 95) {
+            this.game.completed.open()
+            this.game.level.completed = true
+            this.game.level.write()
+        }
+    }
+
     handleCollisionX(player) {
-        this.handleCollision()
+        this.handleCollision(player)
     }
 
     handleCollisionY(player) {
-        this.handleCollision()
+        this.handleCollision(player)
     }
 
-    handleCollision() {
+    handleCollision(player) {
         if (this.isActive) return
 
         this.isActive = true
-
-        this.game.completed.open()
-        this.game.level.completed = true
-        this.game.level.write()
     }
 
     draw(ctx) {
